cf788fdbf3bbb129b699d281e0f915c20693d07f
ROBIN FANTINO <robin.fantino.rf@gmail.com>
2024-09-25 14:23:19 +0200

    j'ai terminé la troisième quête Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,4 @@ import { ArticleComponentComponent } from './article-component/article-component
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'wild-blog';
-  text = "Je suis une légende (I Am Legend), publié en 1954, est un roman de science-fiction écrit par Richard Matheson. Ce livre est rapidement devenu une œuvre culte pour son approche unique du genre post-apocalyptique, en mêlant horreur, science-fiction et psychologie. L'influence de Je suis une légende a été immense sur la culture populaire, inspirant des générations d'écrivains, de réalisateurs et de créateurs dans le domaine de l’horreur et de la science-fiction."
 }
--- src/app/article-component/article-component.component.ts
+++ src/app/article-component/article-component.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-article-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './article-component.component.html',
   styleUrl: './article-component.component.scss'
 })
 export class ArticleComponentComponent {
 
-  title: string = "je suis une titre";
-  text: string = "je suis une texte maginfique";
+  article = {
+    title: 'Titre de l\'article',
+    author: 'John Doe',
+    content: 'Voici le contenu de l\'article.',
+    image: 'https://via.placeholder.com/350x150',
+    isPublished: false,
+    comment: ''
+  };
 
+
+  togglePublication(): void {
+    this.article.isPublished = !this.article.isPublished;
+  }
 }
